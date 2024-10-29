6855b07e0b63e112583365aa11af7bc541f3352b
Jonathan <86590925+Jonathan-Jo-me@users.noreply.github.com>
2024-10-29 12:05:47 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,6 +3,6 @@ terraform {
     bucket         = "terraform-statefile-s3bucket-eks"
     key            = "backend/TFSTATE-FILE.tfstate"
     region         = "ap-south-1"
-    dynamodb_table = "Dynamodb-terraform"
+    #dynamodb_table = "Dynamodb-terraform"
   }
 }
